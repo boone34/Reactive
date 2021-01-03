11799981b742f39e49433f1cd7ede976d84434de
Boone <boone@technoir.net>
2021-01-03 13:21:40 -0800

    Add DisposableWrapper

---
+++ Reactive/DisposableWrapper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace TechNoir.Reactive
+{
+    public class DisposableWrapper<T> : IDisposable
+    {
+        private IDisposable _Disposable;
+        
+        protected bool Disposed { get; private set; }
+
+        public T Wrapped { get; }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (Disposed) return;
+
+            if (disposing && _Disposable != null)
+            {
+                _Disposable.Dispose();
+                _Disposable = null;
+            }
+
+            Disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        public DisposableWrapper(T wrapped)
+        {
+            _Disposable = wrapped as IDisposable;;
+            Wrapped     = wrapped;
+        }
+    }
+}
