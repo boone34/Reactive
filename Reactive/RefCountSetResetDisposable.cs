9cf97db7e02705d9c317d4d2952d1bb3ef0bebda
Boone <boone@technoir.net>
2022-01-08 10:21:35 -0800

    Document RefCountSetResetDisposable

--- Reactive/RefCountSetResetDisposable.cs
+++ Reactive/RefCountSetResetDisposable.cs
@@ -3,10 +3,19 @@ using System.Reactive.Disposables;
 
 namespace TechNoir.Reactive
 {
-	public class RefCountSetResetDisposable
+    /// <summary>
+    /// Class RefCountSetResetDisposable.
+    ///
+    /// Implements a reference count which manages a disposable. The first time the reference count is incremented by calling <see cref="Set"/>
+    /// a <em>Set</em> action is invoked if not null.
+    ///
+    /// Each time the <see cref="IDisposable"/> returned from <see cref="Set"/> is disposed the reference count is decremented. Once decremented
+    /// to zero the <em>Reset</em> action is invoked if not null.
+    /// </summary>
+    public class RefCountSetResetDisposable
 	{
 		private readonly object _Lock;
-		private uint _RefCount;
+		private          uint   _RefCount;
 		private readonly Action _Set;
 		private readonly Action _Reset;
 
@@ -23,7 +32,11 @@ namespace TechNoir.Reactive
 			}
 		}
 
-		public IDisposable Set()
+        /// <summary>
+        /// Increments reference count. If reference count == 0 the <em>Set</em> action is invoked if not null.
+        /// </summary>
+        /// <returns><see cref="IDisposable" /> which should be disposed to decrement reference count.</returns>
+        public IDisposable Set()
 		{
 			lock (_Lock)
 			{
@@ -36,10 +49,15 @@ namespace TechNoir.Reactive
 			}
 		}
 
-		public RefCountSetResetDisposable(Action set, Action reset)
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RefCountSetResetDisposable"/> class.
+        /// </summary>
+        /// <param name="set">Action to be performed when this is set the first time.</param>
+        /// <param name="reset">Action to be performed when this is disposed the last time (when reference count == 0).</param>
+        public RefCountSetResetDisposable(Action set, Action reset)
 		{
-			_Lock = new object();
-			_Set = set;
+			_Lock  = new object();
+			_Set   = set;
 			_Reset = reset;
 		}
 	}
